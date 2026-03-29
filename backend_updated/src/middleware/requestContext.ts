4ffe652c04b398231f779a4612aba0e357330df9
ajay35247 <ajay35247@gmail.com>
2026-03-29 16:44:25 +0530

    Delete backend_updated directory

--- backend_updated/src/middleware/requestContext.ts
+++
@@ -1,8 +0,0 @@
-import crypto from 'crypto';
-import type { NextFunction, Request, Response } from 'express';
-
-export function requestContext(req: Request, res: Response, next: NextFunction) {
-  req.requestId = req.header('x-request-id') || crypto.randomUUID();
-  res.setHeader('x-request-id', req.requestId);
-  next();
-}
